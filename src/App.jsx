f01250c403e43c6a56b9abb58cbadae34ec09045
Veldrine Evelia Kaharwa <146930478+Tech-Vexy@users.noreply.github.com>
2024-01-23 11:33:09 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Veldrine Evelia",
+  title: "Student",
+  email: "eveliaveldrine@gmail.com",
+  gitHub: "Tech-Vexy",
+  instagram: "@vexy0015",
+  linkedIn: "www.linkedin.com/in/veldrine-evelia-0a2b711b",
+  medium: "Eveliaveldrine",
+  twitter: "@EVeldrine",
+  youTube: "@eveliaveldrine2836",
 };
 
 const primaryColor = "#4E567E";
